74b058aac3c526e211dd276360f2de383fe65388
Adrian Zankich <adrian@hybridgroup.com>
2014-06-19 18:29:43 -0700

    Add support for ir-attx4

---
+++ examples/ir-attx4.js
@@ -0,0 +1,36 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connection: { name: 'tessel', adaptor: 'tessel', port: 'A' },
+  device: { name: 'ir', driver: 'ir-attx4' },
+
+  work: function(my) {
+    my.ir.on('error', function (err) {
+      console.log(err)
+    });
+
+    my.ir.on('data', function(data) {
+      console.log("Received RX Data: ", data);
+    });
+
+    every((3).seconds(), function() {
+      /*********************************************
+      This infrared module example transmits the
+      power signal sequence of an Insignia brand
+      television every three seconds, while also
+      listening for (and logging) any incoming
+      infrared data.
+      *********************************************/
+      // Make a buffer of on/off durations (each duration is 16 bits)
+      var powerBuffer = new Buffer([0, 178, 255, 168, 0, 12, 255, 246, 0, 13, 255, 225, 0, 13, 255, 224, 0, 12, 255, 246, 0, 12, 255, 246, 0, 13, 255, 247, 0, 13, 255, 247, 0, 13, 255, 224, 0, 12, 255, 224, 0, 13, 255, 247, 0, 13, 255, 224, 0, 12, 255, 246, 0, 12, 255, 246, 0, 12, 255, 246, 0, 12, 255, 246, 0, 13, 255, 247, 0, 13, 255, 224, 0, 12, 255, 224, 0, 13, 255, 225, 0, 13, 255, 224, 0, 12, 255, 246, 0, 12, 255, 246, 0, 13, 255, 247, 0, 13, 255, 247, 0, 13, 255, 246, 0, 12, 255, 246, 0, 12, 255, 246, 0, 12, 255, 246, 0, 12, 255, 224, 0, 13, 255, 224, 0, 12, 255, 224, 0, 12, 255, 224, 0, 12]);
+      // Send the signal at 38 kHz
+      my.ir.sendRawSignal(38, powerBuffer, function(err) {
+        if (err) {
+          console.log("Unable to send signal: ", err);
+        } else {
+          console.log("Signal sent!");
+        }
+      });
+    });
+  }
+}).start();
